dfaf5bf3b68457f687ee765779df8555dc19213a
unknown <nikolajanembo@gmail.com>
2025-09-19 14:15:25 +0200

    Added Borders

--- Assets/Scripts/RecketController.cs
+++ Assets/Scripts/RecketController.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class RecketController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
+    private bool canMoveUp = true;
+    private bool canMoveDown = true;
     private void Start()
     {
         Debug.Log("Igra je pocela");
@@ -12,13 +14,35 @@ public class RecketController : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) && canMoveUp)
         {
             transform.position += new Vector3 (0, speed,0);      
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow) && canMoveDown)
         {
             transform.position -= new Vector3(0, speed, 0);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Top Border"))
+        {
+            canMoveUp = false;
+        }
+        if (collision.CompareTag("Bottom Border"))
+        {
+            canMoveDown = false;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Top Border"))
+        {
+            canMoveUp = true;
+        }
+        if (collision.CompareTag("Bottom Border"))
+        {
+            canMoveDown = true;
         }
     }
 }
--- Assets/Scripts/RecketController1.cs
+++ Assets/Scripts/RecketController1.cs
@@ -3,17 +3,39 @@ using UnityEngine;
 public class RocketController1 : MonoBehaviour
 {
     [SerializeField] private float speed1 = 5f;
-
-    // Update is called once per frame
+    private bool canMoveUp1 = true;
+    private bool canMoveDown1 = true;
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) && canMoveUp1 == true)
         {
             transform.position += new Vector3(0, speed1, 0);
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) && canMoveDown1 == true)
         {
             transform.position -= new Vector3(0, speed1, 0);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Top Border"))
+        {
+            canMoveUp1 = false;
+        }
+        if (collision.CompareTag("Bottom Border"))
+        {
+            canMoveDown1 = false;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Top Border"))
+        {
+            canMoveUp1 = true;
+        }
+        if (collision.CompareTag("Bottom Border"))
+        {
+            canMoveDown1 = true;
         }
     }
 }
