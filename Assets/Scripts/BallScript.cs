4f6aec63bbab5c923202b51e2a69778c83b20b5c
unknown <nikolajanembo@gmail.com>
2025-09-20 17:21:25 +0200

    Added Score

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -1,11 +1,24 @@
-using Unity.VisualScripting;
+using TMPro;
 using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
     [SerializeField] private int direction = 1;
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private TMP_Text WinnerText;
+    private int playerScore1 = 0;
+    private int playerScore2 = 0;
+   
     private bool gameStarted = false;
+    private Vector3 startPos;
+
+
+    private void Start()
+    {
+        scoreText.text = playerScore1 + ":" + playerScore2;
+        scoreText.fontSize = 45; 
+    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && gameStarted == false)
@@ -32,8 +45,30 @@ public class BallScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(collision.gameObject.name);
-        // if ( direction == 1)
         direction = -direction;
+        if (collision.CompareTag("Right Goal"))
+        {
+            Debug.Log("Player 1 Point!");
+            transform.position = startPos;
+            gameStarted = false;
+            playerScore1 += 1;
+            Debug.Log("Player 1 Score "+ playerScore1);
+            scoreText.text = playerScore1 + ":" + playerScore2;
+
+        }
+        if (collision.CompareTag("Left Goal"))
+        {
+            Debug.Log("Player 2 Point!");
+            transform.position = startPos;
+            gameStarted = false;
+            playerScore2 += 1;
+            Debug.Log("Player 2 Score " + playerScore2);
+            scoreText.text = playerScore1 + ":" + playerScore2;
+        }
+        if (playerScore1 == 3)
+        {
+
+        }
     }
+    
 }
